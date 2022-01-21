7dc9aef447b2bf39bf053428866e9bb1bb1c6ab3
angelinaturchyn <angelinaturchyn@gmail.com>
2022-01-21 13:45:28 -0600

    cypress config

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,19 +1,68 @@
 import LoginPage from "../pages/login.page";
 import ProfilePage from "../pages/profile.page";
 
+
 describe('Auth',function(){
     it('successful log in', function (){
         LoginPage.open()
         LoginPage.login('samsmith@gmail.com','Aa123123')
         ProfilePage.iconUser.should('be.visible')
-        // cy.visit('user/login')
-        // cy.get('[qa-id="email"]')
-        //     .type('samsmith@gmail.com')
-        // cy.get('[qa-id="password"]')
-        //     .type('Aa123123')
-        // cy.get('.login-form-button')
-        //     .click()
-        // cy.get('img[alt="avatarIcon"]')
-        //     .should('be.visible')
+        cy.visit('user/login')
+        cy.get('[qa-id="email"]')
+            .type('samsmith@gmail.com')
+        cy.get('[qa-id="password"]')
+            .type('Aa123123')
+        cy.get('.login-form-button')
+            .click()
+        cy.get('img[alt="avatarIcon"]')
+            .should('be.visible')
+    })
+})
+
+describe('Orders', function() {
+    it('Opens Orders', function(){
+        cy.get('[href="/order"]').click()
+        cy.get('[type="button"]')
+          .parents('#root').find('[class="ant-btn ant-btn-primary"]')
+          .should('contain', 'Create Order')
+
+    })
+})
+
+describe('Creates an Order', function() {
+    it('Creates an Order', function(){
+        cy.get('[class="card card-body"]')
+            .find('[class="ant-select-selection-search"]')
+            .children('[name="FirstName"]')
+            .type('Benjamin Button')
+        cy.get('[type="submit"]').click()
+    })
+})
+
+describe('Clients then and wrap m', function() {
+    it('Opens Clients', function(){
+        cy.get('[href="/client"]').click()
+        cy.contains('Create Client').click()
+        cy.get('[qa-id="firstName"]').type('Benjamin Franklin')
+        cy.get('[class="d-flex justify-content-end"]').children('[type="submit"]').click()
+
+    })
+})
+
+describe('First test',function() {
+    it('successful log in', function () {
+      cy.visit('/')
+      cy.contains('Pricing').click()
+
+    //By Tag Name
+      cy.get('input')
+    //By ID
+      cy.get('#inputEmail')
+     // By Class name
+      cy.get('.input-full-width')
+      //By Attribute name
+      cy.get('[placeholder]')
+        //By Attribute name and value
+         cy.get('[placeholder="Email"]')
     })
 })
---
+++ cypress/pages/orders.page.js
@@ -0,0 +1,9 @@
+// import Page from "./page";
+//
+// class OrdersPage extends Page{
+//    get createOrder() {return cy.get("//div[@id='top-menu']//a[@href='/order']")}
+// }
+//
+//      this.createOrder.click()
+//
+// export default new OrdersPage()
